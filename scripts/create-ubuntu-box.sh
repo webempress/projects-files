c2c3d410c754c29325afcb4352ba0b593f5f9c0d
cate-mbp13 <catecarr321@gmail.com>
2017-12-22 16:35:30 -0500

    Finished 1st version of instructions

--- scripts/create-ubuntu-box.sh
+++ scripts/create-ubuntu-box.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Before running this script: Update VB, update vagrant, update vagrant plugins.
+# Before running this script: check VB & vagrant for updates.
+# to check vagrant version: vagrant --version
 
 # make the directory for the new vagrant box
+# assumes that the ~/projects directory exists
 cd ~/projects
-mkdir trusty64_lampv2
-cd trusty64_lampv2
+mkdir trusty64_lamp
+cd trusty64_lamp
 
-# init box
-trusty64_lampv2 $ vagrant init ubuntu/trusty64
+# init vagrant box
+vagrant init ubuntu/trusty64
 
-# next step is to manually edit the vagrantfile for private_network
+# next step is to manually edit the vagrantfile for
+#   private_network
+#   provisioner script call
